df07d3b70bc6ed9d9a4ab4a9a777b4a3f6480db5
hej <dark199174@gmail.com>
2022-02-02 21:39:37 +0900

    Add > Main Design

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -52,14 +52,23 @@ export default function BasicModal() {
             되어 만다라트라 불립니다.
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            핵심이 되는 목표를 설정하고 목표를 이루기 위한 방법과 아이디어를
-            적으며 목표에 접근하기 위한 방법을 구체화할 수 있으며, 그 방법들을
-            잊지 않기 위해 계속 리마인드 할 때 유용하게 사용 가능합니다.
+            핵심이 되는 목표를 설정하고 이루기 위한 방법과 아이디어를 적으며
+            구체화할 수 있으며, 그 방법들을 잊지 않기 위해 계속 되새길 때
+            유용하게 사용 가능합니다.
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            이 만다라트로 유명한 것은 오타니 쇼헤이(大谷翔平) 선수로 어린
-            시절부터 자신의 목표를 정하고 정리해, 실행했다고 합니다.
+            이 만다라트로 유명한 것은 유명한 야구 선수인 오타니
+            쇼헤이(大谷翔平)로 어린 시절부터 자신의 목표를 정하고 정리해,
+            실행했다고 합니다.
           </Typography>
+          <a
+            href="
+            https://www.youtube.com/results?sp=mAEB&search_query=%EB%A7%8C%EB%8B%A4%EB%9D%BC%ED%8A%B8+%EC%9E%91%EC%84%B1%EB%B2%95"
+            sx={{ mt: 2 }}
+            target="_blank"
+          >
+            링크) 오타니 쇼헤이 선수 만다라트
+          </a>
           <Typography
             className="modal-container__title"
             id="modal-modal-title"
@@ -71,8 +80,9 @@ export default function BasicModal() {
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             정 가운데에 최종 목표를 적은 후, 주위에 최종 목표를 달성하기 위한
-            방법 혹은 아이디어를 적습니다. 다음으로 해당 방법을 확장해서 그
-            방법을 달성하기 위한 방법을 적으면 됩니다.
+            세부 방법 혹은 아이디어를 적습니다. 다음으로 세부 방법 또한 각
+            방향에서 확장해서 그것을 달성하기 위한 방법과 아이디어를 적어
+            나갑니다.
           </Typography>
           <a
             href="
